f017ca850c8c43ab57937a03ba00de2b58950e3c
joanne-perez <Joanne.Perez@onerail.io>
2024-09-30 15:00:57 +0800

    Stream message from openai

---
+++ app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText, convertToCoreMessages } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const model = process.env.OPENAI_MODEL || 'gpt-4o-mini'
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: openai(model),
+    messages: convertToCoreMessages(messages),
+  });
+
+  return result.toDataStreamResponse();
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,59 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useRef } from "react";
+import { useChat } from "ai/react";
 
 export default function Home() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  const { messages, input, handleInputChange, handleSubmit, data } = useChat({
+    onResponse: (response) => {
+      if (response.status === 429) {
+        window.alert("You have reached your request limit for the day.");
+        return;
+      }
+    },
+    onError: (error) => {
+      console.error("Something went wrong", error.toString());
+    },
+  });
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className="text">Welcome! Ask me anything!</div>
-        <div className={styles.chatContainer}></div>
+        <div className={styles.chatContainer}>
+          <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+            {data && data.length ? <pre>{JSON.stringify(data, null, 2)}</pre> : ""}
+            {messages.map((m) => (
+              <div key={m.id} className={styles["whitespace-pre-wrap"]}>
+                {m.role === "user" ? "You: " : "Chatty: "}
+                {m.content}
+              </div>
+            ))}
+          </div>
+        </div>
         <div className={styles.inputContainer}>
-          <input type="text" placeholder="Chat away!" />
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <textarea
+              value={input}
+              onChange={handleInputChange}
+              ref={inputRef}
+              placeholder="Chat away!"
+              tabIndex={0}
+              required
+              rows={1}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && !e.shiftKey) {
+                  formRef.current?.requestSubmit();
+                  e.preventDefault();
+                }
+              }}
+            />
+          </form>
         </div>
       </main>
       <footer className={styles.footer}>
